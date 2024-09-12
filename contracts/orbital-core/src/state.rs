c66f927b5e9da143306b76b97ac145277f14efc5
bekauz <bekauz@protonmail.com>
2024-09-12 20:12:09 +0200

    init user registration

--- contracts/orbital-core/src/state.rs
+++ contracts/orbital-core/src/state.rs
@@ -1,6 +1,26 @@
+use std::collections::HashMap;
+
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Uint64};
 use cw_storage_plus::Map;
+
+/// map of users with their respective configurations
+pub const USER_CONFIGS: Map<Addr, UserConfig> = Map::new("user_configs");
+
+#[cw_serde]
+pub struct UserConfig {
+    // TODO: this may make more sense to have as a top level map
+    // with composite keys of (user, domain) -> clearing_addr
+    pub clearing_accounts: HashMap<String, String>,
+}
+
+impl Default for UserConfig {
+    fn default() -> Self {
+        UserConfig {
+            clearing_accounts: HashMap::new(),
+        }
+    }
+}
 
 /// map of registered remote domains and their configuration
 pub const ORBITAL_DOMAINS: Map<String, OrbitalDomainConfig> = Map::new("domains");
@@ -8,6 +28,7 @@ pub const ORBITAL_DOMAINS: Map<String, OrbitalDomainConfig> = Map::new("domains"
 /// remote domain configuration config which supports different types of account implementations.
 /// currently supported types:
 /// - Polytone: cw-based account implementation that operates via note contract on the origin chain
+/// - ICA: interchain account implementation based on ICS-27
 #[cw_serde]
 pub enum OrbitalDomainConfig {
     Polytone {
